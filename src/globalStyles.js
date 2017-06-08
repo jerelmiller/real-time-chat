f7e7b3d644a1701fef33afd08e6b88ed71ba8201
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 16:57:56 -0700

    Add some global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,12 @@
+import { injectGlobal } from 'styled-components'
+
+// eslint-disable-next-line
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700');
+
+  body {
+    margin: 0;
+    background: #F7FAFB;
+    font-family: Roboto, sans-serif;
+  }
+`
