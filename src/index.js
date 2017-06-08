1aa04238a18a0229a1a9969e7226617b3390132c
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 16:29:28 -0700

    Housekeeping to remove boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App socket={ socket } />,
+  document.getElementById('root')
+)
+registerServiceWorker()
