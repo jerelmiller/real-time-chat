bd71c82b47fb1324fa6768019b8f4a7792831e5f
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 16:30:20 -0700

    Create a socket connection when launching the app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import io from 'socket.io-client'
+
+const socket = io()
 
 ReactDOM.render(
   <App socket={ socket } />,
