7a23ac42cc7b5c17f1d16e953e84798368343c22
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 18:02:04 -0700

    Vertically center and only make chat container a certain height

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -4,9 +4,12 @@ const ChatContainer = styled.div`
   padding: 2rem;
   width: 50%;
   margin: 0 auto;
-  height: 100%;
+  height: 90%;
   border: 1px solid #ddd;
   border-radius: 5px;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
 `
 
 export default ChatContainer
