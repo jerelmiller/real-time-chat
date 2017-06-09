dc88be8b4aef146d600b8c4f3fbb24dd3ef1f4ee
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 22:03:20 -0700

    Move scroll behavior to own component

---
+++ src/components/AutoScroll.js
@@ -0,0 +1,20 @@
+import { findDOMNode } from 'react-dom'
+import React, { Component } from 'react'
+
+export default class AutoScroll extends Component {
+  componentWillUpdate() {
+    const node = findDOMNode(this)
+    this.shouldScroll = node.scrollTop + node.offsetHeight === node.scrollHeight
+  }
+
+  componentDidUpdate() {
+    if (this.shouldScroll) {
+      const node = findDOMNode(this)
+      node.scrollTop = node.scrollHeight
+    }
+  }
+
+  render() {
+    return this.props.children
+  }
+}
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,41 +1,25 @@
+import AutoScroll from './AutoScroll'
 import { CSSTransitionGroup } from 'react-transition-group'
 import MessageListContainer from './MessageListContainer'
 import MessageListItem from './MessageListItem'
-import { findDOMNode } from 'react-dom'
-import React, { Component } from 'react'
+import React from 'react'
 
-class MessageList extends Component {
-  componentWillUpdate() {
-    const node = findDOMNode(this)
-    this.shouldScroll = node.scrollTop + node.offsetHeight === node.scrollHeight
-  }
-
-  componentDidUpdate() {
-    if (this.shouldScroll) {
-      const node = findDOMNode(this)
-      node.scrollTop = node.scrollHeight
-    }
-  }
-
-  render() {
-    const { messages } = this.props
-
-    return (
-      <MessageListContainer>
-        <CSSTransitionGroup
-          transitionName='message-list'
-          transitionEnterTimeout={ 300 }
-        >
-          { messages.map(message => (
-            <MessageListItem
-              key={ message.timestamp }
-              message={ message }
-            />
-          )) }
-        </CSSTransitionGroup>
-      </MessageListContainer>
-    )
-  }
-}
+const MessageList = ({ messages }) => (
+  <AutoScroll>
+    <MessageListContainer>
+      <CSSTransitionGroup
+        transitionName='message-list'
+        transitionEnterTimeout={ 300 }
+      >
+        { messages.map(message => (
+          <MessageListItem
+            key={ message.timestamp }
+            message={ message }
+          />
+        )) }
+      </CSSTransitionGroup>
+    </MessageListContainer>
+  </AutoScroll>
+)
 
 export default MessageList
