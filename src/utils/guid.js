8b9d8745029881f515074c76ea169b751ccdcd81
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 17:48:01 -0700

    rename guid function and export as default

--- src/utils/guid.js
+++ src/utils/guid.js
@@ -1,6 +1,8 @@
-export const createGUID = () =>
+const guid = () =>
   'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = Math.random() * 16 | 0,
     const v = c == 'x' ? r : (r&0x3 | 0x8)
     return v.toString(16)
   })
+
+export default guid
