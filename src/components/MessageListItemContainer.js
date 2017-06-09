7f8b63e3e4f7efde608aeef9e092e2ee45f1c63a
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 23:59:17 -0700

    Fix leave animations. Enforce opacity to ensure no studder

--- src/components/MessageListItemContainer.js
+++ src/components/MessageListItemContainer.js
@@ -4,8 +4,8 @@ const animationTiming = '0.3s cubic-bezier(0.3, 0.79, 0.46, 0.98)'
 
 export default styled.div`
   clear: both;
-  float: ${({ align }) => align};
-  text-align: ${({ align }) => align};
+  float: ${({ align }) => align || 'left'};
+  text-align: ${({ align }) => align || 'left'};
   max-width: 60%;
   margin-bottom: 1rem;
 
@@ -15,9 +15,14 @@ export default styled.div`
     `}
   }
 
-  ${({ leaveAnimiation }) => leaveAnimiation && css`
+  ${({ leaveAnimation }) => leaveAnimation && css`
+    &.message-list-leave {
+      opacity: 1;
+    }
     &.message-list-leave-active {
-      animation: ${leaveAnimiation} ${animationTiming};
+      transition: opacity 0.3s ease-out;
+      animation: ${leaveAnimation} ${animationTiming};
+      opacity: 0;
     `}
   }
 
