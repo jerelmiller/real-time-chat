f92ea5d3ce2aeb7263d11f7800c44e44ab0c8442
Jerel Miller <jerelmiller@gmail.com>
2017-06-09 00:01:20 -0700

    Add scale animations for typing indicator

--- src/components/TypingIndicator.js
+++ src/components/TypingIndicator.js
@@ -3,9 +3,13 @@ import MessageBubble from './MessageBubble'
 import MessageInfo from './MessageInfo'
 import MessageListItemContainer from './MessageListItemContainer'
 import TypingCircles from './TypingCircles'
+import { scaleIn, scaleOut } from '../animations'
 
 const TypingIndicator = ({ name }) => (
-  <MessageListItemContainer>
+  <MessageListItemContainer
+    enterAnimation={ scaleIn }
+    leaveAnimation={ scaleOut }
+  >
     <MessageBubble>
       <TypingCircles />
     </MessageBubble>
