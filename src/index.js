42c4963045fc7cccfcf68c886771e547654e68b3
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 17:53:57 -0700

    Assign an id to the app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import io from 'socket.io-client'
+import guid from './utils/guid'
 import './globalStyles'
 
 const socket = io(process.env.REACT_APP_SOCKET_ADDRESS)
+const id = guid()
 
 ReactDOM.render(
-  <App socket={ socket } />,
+  <App socket={ socket } id={ id } />,
   document.getElementById('root')
 )
 registerServiceWorker()
