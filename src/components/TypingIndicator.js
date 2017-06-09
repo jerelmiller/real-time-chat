1228d1ec5478f431ccc262800867c5a981162ecc
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 22:53:58 -0700

    Add info about who is typing to typing indicator

--- src/components/TypingIndicator.js
+++ src/components/TypingIndicator.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import MessageBubble from './MessageBubble'
+import MessageInfo from './MessageInfo'
+import MessageListItemContainer from './MessageListItemContainer'
 import TypingCircles from './TypingCircles'
+import styled from 'styled-components'
 
 const TypingIndicator = () => (
-  <MessageBubble>
-    <TypingCircles />
-  </MessageBubble>
+  <MessageListItemContainer>
+    <MessageBubble>
+      <TypingCircles />
+    </MessageBubble>
+    <MessageInfo>Jerel is typing...</MessageInfo>
+  </MessageListItemContainer>
 )
 
 export default TypingIndicator
