8f857fe5b846fb3867c554874ac3aabdfc0b297e
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 17:20:58 -0700

    Add a ChatContainer component

---
+++ src/components/ChatContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const ChatContainer = styled.div`
+  padding: 2rem;
+  width: 50%;
+  margin: 0 auto;
+  height: 100%;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`
+
+export default ChatContainer
