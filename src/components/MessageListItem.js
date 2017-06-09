5473eec2ed5b2c637646cc150e9b1f9aae775031
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 23:44:26 -0700

    More flexibility in animations for list item container

--- src/components/MessageListItem.js
+++ src/components/MessageListItem.js
@@ -3,11 +3,15 @@ import MessageBubble from './MessageBubble'
 import MessageListItemContainer from './MessageListItemContainer'
 import MessageInfo from './MessageInfo'
 import moment from 'moment'
+import { slideFromLeft, slideFromRight } from '../animations'
 
 const DATE_FORMAT = 'MMM Do, h:mma'
 
 const MessageListItem = ({ message: { mine, message, name, timestamp }}) => (
-  <MessageListItemContainer mine={ mine }>
+  <MessageListItemContainer
+    align={ mine ? 'right' : 'left' }
+    enterAnimation={ mine ? slideFromRight : slideFromLeft }
+  >
     <MessageBubble mine={ mine }>
       { message }
     </MessageBubble>
--- src/components/MessageListItemContainer.js
+++ src/components/MessageListItemContainer.js
@@ -1,40 +1,24 @@
-import styled, { css, keyframes } from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const slideStrength = '20%'
-
-const theirMessage = keyframes`
-  from {
-    transform: translate3d(-${slideStrength}, 0, 0);
-    opacity: 0;
-  }
-
-  to {
-    transform: translate3d(0, 0, 0);
-    opacity: 1;
-  }
-`
-
-const myMessage = keyframes`
-  from {
-    transform: translate3d(${slideStrength}, 0, 0);
-    opacity: 0;
-  }
-
-  to {
-    transform: scale(1) translate3d(0);
-    opacity: 1;
-  }
-`
+const animationTiming = '0.3s cubic-bezier(0.3, 0.79, 0.46, 0.98)'
 
 export default styled.div`
   clear: both;
-  float: left;
+  float: ${({ align }) => align};
+  text-align: ${({ align }) => align};
   max-width: 60%;
   margin-bottom: 1rem;
 
-  &.message-list-enter-active {
-    animation: ${({ mine }) => mine ? myMessage : theirMessage }
-      0.3s cubic-bezier(0.3, 0.79, 0.46, 0.98);
+  ${({ enterAnimation }) => enterAnimation && css`
+    &.message-list-enter-active {
+      animation: ${enterAnimation} ${animationTiming};
+    `}
+  }
+
+  ${({ leaveAnimiation }) => leaveAnimiation && css`
+    &.message-list-leave-active {
+      animation: ${leaveAnimiation} ${animationTiming};
+    `}
   }
 
   ${({ mine }) => mine && css`
