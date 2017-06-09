1d7df07a645e0d6cbd123a8bc6581f1f8f4333f9
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 20:30:30 -0700

    Tweak placeholder styles

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
 
 const Input = styled.input`
-  font-weight: 300;
+  font-weight: 700;
   width: 100%;
   padding: 1rem;
   font-size: 1rem;
 
   &:focus { outline: none; }
+  &::placeholder {
+    opacity: 0.5;
+    font-weight: 300;
+  }
 `
 
 export default Input
