5b20719babaf1f9b24cbc980c2bcce8a0395ae30
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 18:33:23 -0700

    Move keydown to message input and instead take on onSubmit prop

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -2,9 +2,19 @@ import React from 'react'
 import Input from './Input'
 import MessageInputContainer from './MessageInputContainer'
 
+const ENTER_KEY = 13
+
 const MessageInput = props => (
   <MessageInputContainer>
-    <Input type='text' { ...props } />
+    <Input
+      { ...props }
+      type='text'
+      onKeyDown={ e => {
+        if (e.which === ENTER_KEY) {
+          props.onSubmit()
+        }
+      }}
+    />
   </MessageInputContainer>
 )
 
