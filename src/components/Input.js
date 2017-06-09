ac71522d03f1750afc3437bfc2993a51ce7c2ea0
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 18:23:15 -0700

    Define an Input component

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+  font-weight: 300;
+  width: 100%;
+  padding: 1rem;
+  font-size: 1rem;
+
+  &:focus { outline: none; }
+`
+
+export default Input
