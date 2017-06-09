636ab7aa5cfa947bb387a24deb0bdf514a06c383
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 20:13:41 -0700

    Add MessageTextInput

---
+++ src/components/MessageTextInput.js
@@ -0,0 +1,9 @@
+import Input from './Input'
+
+export default Input.extend`
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  border: none;
+  border-top: 1px solid #ddd;
+  padding-right: 3rem;
+`
