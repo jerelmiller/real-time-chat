c1a15248ac0fcc986970a6422b0ab77f5031a967
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 21:48:20 -0700

    Add enter animation

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,15 +1,21 @@
+import { CSSTransitionGroup } from 'react-transition-group'
 import MessageListContainer from './MessageListContainer'
 import MessageListItem from './MessageListItem'
 import React from 'react'
 
 const MessageList = ({ messages }) => (
   <MessageListContainer>
-    { messages.map(message => (
-      <MessageListItem
-        key={ message.timestamp }
-        message={ message }
-      />
-    )) }
+    <CSSTransitionGroup
+      transitionName='message-list'
+      transitionEnterTimeout={ 300 }
+    >
+      { messages.map(message => (
+        <MessageListItem
+          key={ message.timestamp }
+          message={ message }
+        />
+      )) }
+    </CSSTransitionGroup>
   </MessageListContainer>
 )
 
--- src/components/MessageListItemContainer.js
+++ src/components/MessageListItemContainer.js
@@ -1,10 +1,41 @@
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+const slideStrength = '20%'
+
+const theirMessage = keyframes`
+  from {
+    transform: translate3d(-${slideStrength}, 0, 0);
+    opacity: 0;
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+`
+
+const myMessage = keyframes`
+  from {
+    transform: translate3d(${slideStrength}, 0, 0);
+    opacity: 0;
+  }
+
+  to {
+    transform: scale(1) translate3d(0);
+    opacity: 1;
+  }
+`
 
 export default styled.div`
   clear: both;
   float: left;
   max-width: 60%;
   margin-bottom: 1rem;
+
+  &.message-list-enter-active {
+    animation: ${({ mine }) => mine ? myMessage : theirMessage }
+      0.3s cubic-bezier(0.3, 0.79, 0.46, 0.98);
+  }
 
   ${({ mine }) => mine && css`
     float: right;
