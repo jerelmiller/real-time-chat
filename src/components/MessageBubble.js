d195fb58ad7c6314517ebca22e56ce8825e589df
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 19:54:42 -0700

    Add arrow styles on MessageBubble

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -10,10 +10,32 @@ const MessageBubble = styled.div`
   max-width: 60%;
   background: #eee;
   float: left;
+  position: relative;
+
+  &:after {
+    content: '';
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+    border: 10px solid transparent;
+    border-right-color: #eee;
+    right: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
   ${({ mine }) => mine && css`
     color: white;
     background: #6F5A98;
     float: right;
+
+    &:after {
+      border-left-color: #6F5A98;
+      border-right-color: transparent;
+      right: initial;
+      left: 100%;
+    }
   `}
 `
 
