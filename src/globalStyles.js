7764a8723ffc0e2de93c187f10031d7407fabf17
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 17:04:52 -0700

    Add global box-sizing

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -4,6 +4,13 @@ import { injectGlobal } from 'styled-components'
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700');
 
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
   body {
     margin: 0;
     background: #F7FAFB;
