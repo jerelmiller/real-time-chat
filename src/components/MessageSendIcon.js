ec28fac42ded3364d986f8bdefd537aa355d6440
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 20:13:47 -0700

    Add MessageSendIcon

---
+++ src/components/MessageSendIcon.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import PaperPlaneIcon from 'react-icons/lib/fa/paper-plane-o'
+
+export default styled(PaperPlaneIcon)`
+  position: absolute;
+  right: 1rem;
+  top: 50%;
+  transform: translateY(-50%);
+  fill: #999;
+  cursor: pointer;
+`
