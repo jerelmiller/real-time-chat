e60ea23ad3d6744c706ff5f7d4bc2b1ef547f78c
Jerel Miller <jerelmiller@gmail.com>
2017-06-08 21:59:43 -0700

    Auto scroll list when new messages come in

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,22 +1,41 @@
 import { CSSTransitionGroup } from 'react-transition-group'
 import MessageListContainer from './MessageListContainer'
 import MessageListItem from './MessageListItem'
-import React from 'react'
+import { findDOMNode } from 'react-dom'
+import React, { Component } from 'react'
 
-const MessageList = ({ messages }) => (
-  <MessageListContainer>
-    <CSSTransitionGroup
-      transitionName='message-list'
-      transitionEnterTimeout={ 300 }
-    >
-      { messages.map(message => (
-        <MessageListItem
-          key={ message.timestamp }
-          message={ message }
-        />
-      )) }
-    </CSSTransitionGroup>
-  </MessageListContainer>
-)
+class MessageList extends Component {
+  componentWillUpdate() {
+    const node = findDOMNode(this)
+    this.shouldScroll = node.scrollTop + node.offsetHeight === node.scrollHeight
+  }
+
+  componentDidUpdate() {
+    if (this.shouldScroll) {
+      const node = findDOMNode(this)
+      node.scrollTop = node.scrollHeight
+    }
+  }
+
+  render() {
+    const { messages } = this.props
+
+    return (
+      <MessageListContainer>
+        <CSSTransitionGroup
+          transitionName='message-list'
+          transitionEnterTimeout={ 300 }
+        >
+          { messages.map(message => (
+            <MessageListItem
+              key={ message.timestamp }
+              message={ message }
+            />
+          )) }
+        </CSSTransitionGroup>
+      </MessageListContainer>
+    )
+  }
+}
 
 export default MessageList
